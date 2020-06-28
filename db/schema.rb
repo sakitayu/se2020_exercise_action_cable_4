7e12d0e1dcd39dc2a64af4ffb53ee1b3f6959ae8
sakitayu <sakitakitakita@gmail.com>
2020-06-29 00:26:09 +0900

    セキュアなパスワードの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_145721) do
+ActiveRecord::Schema.define(version: 2020_06_28_151315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_145721) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
